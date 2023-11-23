bb62e6a46fd6f8c13b72a785071a330ac2fee83c
Rafael Nascimento <rafael124715@gmail.com>
2023-11-23 11:40:32 -0300

    Leitura de dados de usuários

---
+++ main.py
@@ -0,0 +1,13 @@
+from user import User
+from interface import PyNews
+
+usuarios = []
+with open('usuarios.txt', 'r') as file:
+    for line in file:
+        usuario, nome, senha = line.strip().split(' - ')
+        usr = User(usuario, nome, senha)
+        usuarios.append(usr)
+
+gui = PyNews(usuarios)
+
+gui.login()
