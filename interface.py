c4494dfc5f220fccea7a323e16cfd7702a6ab65c
Rafael Nascimento <rafael124715@gmail.com>
2023-11-24 11:45:46 -0300

    Melhoras de Interface e Mensagens de Erro

--- interface.py
+++ interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox as mg
 from newsapi import NewsApiClient
 
 
@@ -25,22 +26,27 @@ class Interface:
 
                     else:
                         print("Senha incorreta")
+                        mg.showinfo("Falha no Login", "Senha Incorreta!", icon='error')
 
             if not encontrado:
                 print("Usuário Inexistente")
+                mg.showinfo("Falha no Login", "Usuário não encontrado!", icon='error')
 
         # Login window
         login = Tk()
         login.title("Login de Usuário")
-        login.geometry("350x400")
+        login.geometry("350x200")
+
+        frame_login = Frame(login)
+        frame_login.pack()
 
         # widgets
-        login_label = Label(login, text="Login")
-        username_label = Label(login, text="Usuário")
-        username_entry = Entry(login)
-        password_label = Label(login, text="Senha")
-        password_entry = Entry(login, show='*')
-        login_button = Button(login, text='Entrar', command=checklogin)
+        login_label = Label(frame_login, text="Login")
+        username_label = Label(frame_login, text="Usuário")
+        username_entry = Entry(frame_login)
+        password_label = Label(frame_login, text="Senha")
+        password_entry = Entry(frame_login, show='*')
+        login_button = Button(frame_login, text='Entrar', command=checklogin)
 
         # Placing widgets
         login_label.grid(row=0, column=0, columnspan=2)
@@ -66,8 +72,9 @@ class Interface:
 class PyNews(Interface):
     def __init__(self, usuarios):
         super().__init__(usuarios)
-        self.key = "f0092823669c4b4d8e260b1eb30cfc4d"
+        self.key = "d4bb5b7f812541389fa49cabc50659d0"
         self.api = NewsApiClient(api_key=self.key)
+        self.q = 'manchete'
 
     def acess(self):
         self.news()
@@ -77,17 +84,24 @@ class PyNews(Interface):
         intro_usario = f"Bem vindo {nome}"
 
         def getnews():
-            interesse = 'Mundo'
             busca = search_entry.get()
             from_colected = from_date_entry.get()
             to_colected = to_date_entry.get()
+            old_date = False
 
+            # Oldest news 2023-10-23
             if from_colected != '':
                 from_dia, from_mes = from_colected.strip().split('/')
                 if len(from_dia) == 1:
                     from_dia = '0' + from_dia
 
                 from_data = f"2023-{from_mes}-{from_dia}"
+
+                if int(from_mes) < 10:
+                    old_date = True
+                elif int(from_dia) < 23:
+                    old_date = True
+
             else:
                 from_data = None
 
@@ -101,10 +115,13 @@ class PyNews(Interface):
                 to_data = None
 
             if busca != '':
-                interesse = busca
+                self.q = busca
+
+            if old_date:
+                mg.showinfo("Data Inválida", "Notícia mais antiga: 23/10")
 
             # Headlines
-            news = self.api.get_everything(q=interesse, language='pt', from_param=from_data, to=to_data)
+            news = self.api.get_everything(q=self.q, language='pt', from_param=from_data, to=to_data)
 
             # Clear
             text.delete(1.0, END)
@@ -126,40 +143,48 @@ class PyNews(Interface):
         # News Window
         win = Tk()
         win.title('News App')
+        news_frame = Frame(win)
+        search_frame = Frame(win)
+        date_frame = Frame(win)
+
+        # Frames
+        news_frame.grid(row=0, column=0, columnspan=2)
+        search_frame.grid(row=1, column=1)
+        date_frame.grid(row=1, column=0)
 
         # Intro
-        intro = Label(win, text=intro_usario)
+        intro = Label(news_frame, text=intro_usario)
 
         # News Text
-        text = Text(win, height=20, width=150)
+        text = Text(news_frame, height=20, width=125)
 
         # Search Context
-        search_label = Label(win, text="Assunto")
-        search_entry = Entry(win)
+        search_label = Label(search_frame, text="Assunto")
+        search_entry = Entry(search_frame)
 
         # From Date
-        from_date_label = Label(win, text="Inicio (dia/mes)")
-        from_date_entry = Entry(win)
+        from_date_label = Label(date_frame, text="Inicio (dia/mes)")
+        from_date_entry = Entry(date_frame)
 
         # To Date
-        to_date_label = Label(win, text="Fim (dia/mes)")
-        to_date_entry = Entry(win)
+        to_date_label = Label(date_frame, text="Fim (dia/mes)")
+        to_date_entry = Entry(date_frame)
 
         # Buttons
-        enter = Button(win, text="Procurar", command=getnews)
+        enter = Button(search_frame, text="Procurar", command=getnews)
 
         # Layout
-        intro.grid(row=0, column=0, columnspan=4)
-        text.grid(row=1, column=0, columnspan=4)
+        intro.grid(row=0, column=0)
+        text.grid(row=1, column=0)
 
-        from_date_label.grid(row=2, column=0)
-        from_date_entry.grid(row=2, column=1)
+        from_date_label.grid(row=0, column=0)
+        from_date_entry.grid(row=0, column=1)
 
-        to_date_label.grid(row=2, column=2)
-        to_date_entry.grid(row=2, column=3)
+        to_date_label.grid(row=1, column=0)
+        to_date_entry.grid(row=1, column=1)
 
-        search_label.grid(row=3, column=0)
-        search_entry.grid(row=3, column=1)
-        enter.grid(row=4, column=0, columnspan=4)
+        search_label.grid(row=0, column=0)
+        search_entry.grid(row=0, column=1)
+        enter.grid(row=1, column=0, columnspan=2)
 
         win.mainloop()
